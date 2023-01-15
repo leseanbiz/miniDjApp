0c6751ff51b11b1f5ec92b7bc72f1412a02fc895
LeSean Johnson <leseanbiz@gmail.com>
2023-01-14 20:19:32 -0800

    clearing out unused files/code

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,4 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import whiteboardReducer, {
   IInitialState,
 } from "../features/whiteBoard/whiteBoardSlice";
@@ -20,7 +19,6 @@ const liveblocksEnhancerConfig = {
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     whiteboard: whiteboardReducer,
     mixer: mixerReducer,
   },
